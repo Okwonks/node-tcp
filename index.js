81574b1d8c36d914fa25d9b29e509e17f2834930
Albert Oketch <albert@paygoenergy.co>
2020-08-04 13:05:22 +0300

    add tcp chat functionality

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const net = require('net');
 const logger = require('./logger');
 const log = logger();
 
-const server = net.createServer(socket => {
+let clients = [];
+
+const server = net.createServer();
+
+server.on('connection', socket => {
+  log.info('Server connected...');
+  clients.push(socket);
+
+  socket.setEncoding('utf8');
+
   socket.on('error', err => log.error(err.message));
 
   socket.on('data', async data => {
+    clients.forEach((client, idx) => {
+      if(clients[idx] !== client) {
+        clients[idx].write(data);
+      }
+    });
+
     const received = data.toString().trim();
-    log.info('REQUEST RECEIVED:', received);
-    socket.end(`thanks for saying ${received}.`);
+    log.info(`Data received from client: ${socket}`, received);
   });
+
+  socket.on('end', () => clients.splice(clients.indexOf(socket), 1));
 });
 
 server.listen(8080, '0.0.0.0', () => log.info('Server listening on port 8080'));
